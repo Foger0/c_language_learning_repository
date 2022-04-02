9a7b8b84520dcb85b587800416ae9a639e666c68
lower-fall-in-the-sea <1056981071@qq.com>
2022-04-02 21:02:32 +0800

    11111

---
+++ Project3/Project3/源.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+#include<stdio.h>
+int main()
+{
+    char a[] = "**";
+    char c[] = "*  *";
+    char b[] = "************";
+        printf("%6s\n", a);
+        printf("%6s\n", a);
+        printf("%s\n", b);
+        printf("%s\n", b);
+        printf("%7s\n", c);
+        printf("%7s\n", c);
+
+
+        return 0;
+}
